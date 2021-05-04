e3ccd489d9f260b0a4659e79044e77836d2aec3b
ADiks <antondikson@gmail.com>
2021-05-04 11:21:05 +0300

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd
+} from "@react-firebase/auth";
+import { firebaseConfig } from './firebaseConfig'
+import {SignInForm} from "./components/Form";
+import {Switch, BrowserRouter as Router, Route} from "react-router-dom";
 
-function App() {
+const Routing = () => (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/courses/:id">
+            course id
+          </Route>
+          <Route path="/profile">
+            profile
+          </Route>
+          <Route path="/">
+            courses
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+)
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+        <FirebaseAuthConsumer>
+          {({ isSignedIn }) => {
+            return isSignedIn ? <Routing/> : <SignInForm/>
+          }}
+        </FirebaseAuthConsumer>
+      </FirebaseAuthProvider>
+      // <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+      //   <div>
+      //     <button
+      //         onClick={() => {
+      //           const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+      //           firebase.auth().signInWithPopup(googleAuthProvider);
+      //         }}
+      //     >
+      //       Sign In with Google
+      //     </button>
+      //     <button
+      //         data-testid="signin-anon"
+      //         onClick={() => {
+      //           firebase.auth().signInAnonymously();
+      //         }}
+      //     >
+      //       Sign In Anonymously
+      //     </button>
+      //     <button
+      //         onClick={() => {
+      //           firebase.auth().signOut();
+      //         }}
+      //     >
+      //       Sign Out
+      //     </button>
+      //     <FirebaseAuthConsumer>
+      //       {({ isSignedIn, user, providerId }) => {
+      //         return (
+      //             <pre style={{ height: 300, overflow: "auto" }}>
+      //           {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+      //         </pre>
+      //         );
+      //       }}
+      //     </FirebaseAuthConsumer>
+      //     <div>
+      //       <IfFirebaseAuthed>
+      //         {() => {
+      //           return <div>You are authenticated</div>;
+      //         }}
+      //       </IfFirebaseAuthed>
+      //       <IfFirebaseAuthedAnd
+      //           filter={({ providerId }) => providerId !== "anonymous"}
+      //       >
+      //         {({ providerId }) => {
+      //           return <div>You are authenticated with {providerId}</div>;
+      //         }}
+      //       </IfFirebaseAuthedAnd>
+      //     </div>
+      //   </div>
+      // </FirebaseAuthProvider>
   );
-}
-
-export default App;
+};
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSBLS65J_diS21H7tyMvm69P9CwLp73Vk",
+  authDomain: "idemy-react.firebaseapp.com",
+  projectId: "idemy-react",
+  storageBucket: "idemy-react.appspot.com",
+  messagingSenderId: "70838068635",
+  appId: "1:70838068635:web:d13b97ef10b12570606d3e"
+}
